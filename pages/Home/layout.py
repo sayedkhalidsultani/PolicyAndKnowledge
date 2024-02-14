94e1c7a6c1f7bbdc3507c212ef47d1bcfc12c514
SayedKhalidSultani <sayed-khalid.sultani@undp.org>
2024-02-14 14:16:08 +0430

    reverse back as pyodc is not supported

--- pages/Home/layout.py
+++
@@ -1,9 +0,0 @@
-from dash import html
-import dash
-
-dash.register_page(__name__, path='/home')
-
-layout = html.Div([
-     html.P('Welcome to the Home page...'),
-    # More components for Page 1
-])
--- pages/mande/layout.py
+++
@@ -1,9 +0,0 @@
-from dash import html
-import dash
-
-dash.register_page(__name__,path='/mande')
-
-layout = html.Div([
-    html.H1('mande  Page Welcome sultani'),
-    # More components for Page 1
-])
